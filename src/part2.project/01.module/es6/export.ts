bf9a7f88cd1e5eda7886250da50b21dcf2fb7508
CcccFz <ccccfz@163.com>
2022-12-30 10:07:57 +0800

    feat: import vs export

---
+++ src/part2.project/01.module/es6/export.ts
@@ -0,0 +1,10 @@
+export let a = 1
+export function setA(x: number) {
+  a = x
+}
+
+let b = 'b'
+export function setB(y: string) {
+  b = y
+}
+export default b
---
+++ src/part2.project/01.module/es6/import.ts
@@ -0,0 +1,9 @@
+import y, {a as x, setA, setB} from './export'
+
+console.log(x);
+setA(2)
+console.log(x);
+
+console.log(y);
+setB('c')
+console.log(y);
